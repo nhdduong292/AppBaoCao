69d54b26d62bb7f0031086882618ee42b68734d8
DUONG <huuduong996@gmail.com>
2018-04-19 15:13:24 +0700

    Initial commit

---
+++ app/src/main/java/duongnh/com/appbaocao/fragment/LoginFragment.java
@@ -0,0 +1,10 @@
+package duongnh.com.appbaocao.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Admin on 4/19/2018.
+ */
+
+public class LoginFragment extends Fragment {
+}
